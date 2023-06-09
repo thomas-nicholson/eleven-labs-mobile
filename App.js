57bd7f4456159d829ce14dcbc19fda1df54b2986
thomas-nicholson <tnicholson128@protonmail.com>
2023-06-09 01:39:01 +0000

    Corrected App.js formatting

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Button, View } from'react-native';
-
+import { Button, View } from 'react-native';
 
 import HistoryPage from './HistoryPage'; // make sure the path is correct
 import HomePage from './HomePage';
@@ -16,22 +15,23 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="ElevenLabs" component={HomePage} 
+        <Stack.Screen name="ElevenLabs" component={HomePage}
           options={({ navigation }) => ({
-          headerRight: () => (
-            <View style={{ flexDirection: 'row' }}>
-              <Button
-              onPress={() => navigation.navigate('User')}
-              title="User"
-              color="#000"
-            />
-              <Button
-              onPress={() => navigation.navigate('History')}
-              title="History"
-              color="#000"
-            />
-            </View>
-            )})} />
+            headerRight: () => (
+              <View style={{ flexDirection: 'row' }}>
+                <Button
+                  onPress={() => navigation.navigate('User')}
+                  title="User"
+                  color="#000"
+                />
+                <Button
+                  onPress={() => navigation.navigate('History')}
+                  title="History"
+                  color="#000"
+                />
+              </View>
+            )
+          })} />
         <Stack.Screen name="History" component={HistoryPage} />
         <Stack.Screen name="User" component={UserPage} />
       </Stack.Navigator>
