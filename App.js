b4f00cf6e0814088f996b71a55493efd2bc3dd1e
thomas-nicholson <tnicholson128@protonmail.com>
2023-06-09 01:37:23 +0000

    Updated App.js to add functionality to header buttons and navigation to user page

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Button, View } from'react-native';
 
 import HistoryPage from './HistoryPage'; // make sure the path is correct
 import HomePage from './HomePage';
+import UserPage from './UserPage';
 
 const Stack = createStackNavigator();
 
@@ -15,22 +16,24 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="ElevenLabs" component={HomePage} options={{
+        <Stack.Screen name="ElevenLabs" component={HomePage} 
+          options={({ navigation }) => ({
           headerRight: () => (
             <View style={{ flexDirection: 'row' }}>
               <Button
-              onPress={() => alert('This is a button!')}
+              onPress={() => navigation.navigate('User')}
               title="User"
               color="#000"
             />
               <Button
-              onPress={() => alert('This is a button!')}
+              onPress={() => navigation.navigate('History')}
               title="History"
               color="#000"
             />
             </View>
-            )}} />
+            )})} />
         <Stack.Screen name="History" component={HistoryPage} />
+        <Stack.Screen name="User" component={UserPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
